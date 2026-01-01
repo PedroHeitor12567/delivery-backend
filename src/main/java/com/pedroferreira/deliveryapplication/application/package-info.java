06ef2d92fed30247fd7b6b485b4da3d8d9869ecd
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2026-01-01 10:20:45 -0300

    feat: add package info

---
+++ src/main/java/com/pedroferreira/deliveryapplication/application/package-info.java
@@ -0,0 +1,6 @@
+@ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package com.pedroferreira.deliveryapplication.application;
+
+import org.springframework.modulith.ApplicationModule;
---
+++ src/main/java/com/pedroferreira/deliveryapplication/domain/package-info.java
@@ -0,0 +1,6 @@
+@ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package com.pedroferreira.deliveryapplication.domain;
+
+import org.springframework.modulith.ApplicationModule;
