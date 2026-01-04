07caeef4c5bd025aee95b0d766c3cd4f5723f4f4
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2026-01-03 22:33:46 -0300

    feat: dto response para vendedor

--- src/main/java/com/pedroferreira/deliveryapplication/application/dto/response/SellerResponse.java
+++ src/main/java/com/pedroferreira/deliveryapplication/application/dto/response/SellerResponse.java
@@ -1,4 +1,36 @@
 package com.pedroferreira.deliveryapplication.application.dto.response;
 
+import com.pedroferreira.deliveryapplication.domain.entity.Seller;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SellerResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private String cpf;
+    private String phone;
+    private String address;
+    private Boolean active;
+    private StoreSimpleResponse store;
+
+    public static SellerResponse fromEntity(Seller seller) {
+        return SellerResponse.builder()
+                .id(seller.getId())
+                .username(seller.getUsername())
+                .email(seller.getEmail())
+                .cpf(seller.getCpf())
+                .phone(seller.getPhone())
+                .address(seller.getAddress())
+                .active(seller.getActive())
+                .store(seller.getStore() != null ?
+                        StoreSimpleResponse.fromEntity(seller.getStore()) : null)
+                .build();
+    }
 }
