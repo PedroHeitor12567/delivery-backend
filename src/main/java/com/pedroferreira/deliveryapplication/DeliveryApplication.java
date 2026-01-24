e286ee8501994069bd3b27a1f8d56bd21236e762
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2026-01-24 18:19:47 -0300

    fix: arrumando bug de inicialização

--- src/main/java/com/pedroferreira/deliveryapplication/DeliveryApplication.java
+++ src/main/java/com/pedroferreira/deliveryapplication/DeliveryApplication.java
@@ -2,11 +2,9 @@ package com.pedroferreira.deliveryapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.pedroferreira.deliveryapplication.infrastructure.persistence.entity")
 @EnableJpaRepositories("com.pedroferreira.deliveryapplication.infrastructure.repository")
 public class DeliveryApplication {
 
