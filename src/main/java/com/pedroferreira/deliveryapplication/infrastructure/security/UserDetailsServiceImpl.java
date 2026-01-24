b3083eb2d39d172de25a277d1a625d53aa3f88fa
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2026-01-24 18:19:16 -0300

    feat: criando bean userdetails

--- src/main/java/com/pedroferreira/deliveryapplication/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/pedroferreira/deliveryapplication/infrastructure/security/UserDetailsServiceImpl.java
@@ -1,4 +1,29 @@
 package com.pedroferreira.deliveryapplication.infrastructure.security;
 
-public class UserDetailsServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    // Quando tiver repositório:
+    // private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        // 🔴 EXEMPLO TEMPORÁRIO
+        if (!username.equals("admin")) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        return User.builder()
+                .username("admin")
+                .password("{noop}admin123") // apenas teste
+                .roles("ADMIN")
+                .build();
+    }
 }
